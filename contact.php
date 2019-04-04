5461a9be7a67060c4bd9fa1b58ebaaed078f12dd
Jameson Lopp <jameson.lopp@gmail.com>
2019-04-04 09:36:25 -0400

    fix js CSP

--- contact.php
+++ contact.php
@@ -63,17 +63,23 @@ else { document.getElementById(d).style.display = "none"; }
 }
 //--></script>
 
-<!-- Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  <!-- Google Analytics -->
+  <script>
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'js/gastats.js', 'ga');
 
-  ga('create', 'UA-2071667-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+    ga('create', 'UA-2071667-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 
 </head>
 
