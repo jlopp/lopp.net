b1d195b09b6d1d8a1fb8b81b4a42a737fe6c984b
Jameson Lopp <jameson.lopp@gmail.com>
2020-01-11 10:20:56 -0500

    change analytics to load after the rest of the page finishes loading

--- contact.php
+++ contact.php
@@ -159,24 +159,29 @@ NjT4rMUesCnjTVHVM9KXvMemwAhhYbM=
     }
   </script>
 
-  <!-- Google Analytics -->
   <script>
     (function(i, s, o, g, r, a, m) {
       i['GoogleAnalyticsObject'] = r;
       i[r] = i[r] || function() {
         (i[r].q = i[r].q || []).push(arguments)
       }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
+      i.initAnalytics = function() {
+          a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+          a.async = 1;
+          a.src = g;
+          m.parentNode.insertBefore(a, m)
+      }
     })(window, document, 'script', 'js/gastats.js', 'ga');
 
     ga('create', 'UA-2071667-1', 'auto');
     ga('send', 'pageview');
+    function atOnload() {
+        initAnalytics();
+    }
+    if (window.addEventListener) window.addEventListener("load", atOnload, false);
+    else if (window.attachEvent) window.attachEvent("onload", atOnload);
+    else window.onload = atOnload;
   </script>
-
 </head>
 
 <body>
